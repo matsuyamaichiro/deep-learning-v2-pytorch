33f225747bc0ee611b9ea505a1170b7fbae31ba9
matsuyamaichiro <matsuyama.ichiro@gmail.com>
2021-06-20 01:42:17 +0900

    project 1 bike sharing patterns submission

--- project-bikesharing/my_answers.py
+++ project-bikesharing/my_answers.py
@@ -20,7 +20,7 @@ class NeuralNetwork(object):
         #
         # Note: in Python, you can define a function with a lambda expression,
         # as shown below.
-        self.activation_function = lambda x : 0  # Replace 0 with your sigmoid calculation.
+        self.activation_function = lambda x : (1 / (1 + np.exp(-x)))  # Replace 0 with your sigmoid calculation.
         
         ### If the lambda code above is not something you're familiar with,
         # You can uncomment out the following three lines and put your 
@@ -64,12 +64,12 @@ class NeuralNetwork(object):
         #### Implement the forward pass here ####
         ### Forward pass ###
         # TODO: Hidden layer - Replace these values with your calculations.
-        hidden_inputs = None # signals into hidden layer
-        hidden_outputs = None # signals from hidden layer
+        hidden_inputs = np.matmul(X, self.weights_input_to_hidden) # signals into hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs) # signals from hidden layer
 
         # TODO: Output layer - Replace these values with your calculations.
-        final_inputs = None # signals into final output layer
-        final_outputs = None # signals from final output layer
+        final_inputs = np.matmul(hidden_outputs, self.weights_hidden_to_output) # signals into final output layer
+        final_outputs = final_inputs # signals from final output layer
         
         return final_outputs, hidden_outputs
 
@@ -88,21 +88,20 @@ class NeuralNetwork(object):
         ### Backward pass ###
 
         # TODO: Output error - Replace this value with your calculations.
-        error = None # Output layer error is the difference between desired target and actual output.
+        error = y - final_outputs # Output layer error is the difference between desired target and actual output.
         
         # TODO: Calculate the hidden layer's contribution to the error
-        hidden_error = None
+        hidden_error = np.matmul(self.weights_hidden_to_output, error)
         
         # TODO: Backpropagated error terms - Replace these values with your calculations.
-        output_error_term = None
+        output_error_term = error
         
-        hidden_error_term = None
+        hidden_error_term = hidden_error * hidden_outputs * (1 - hidden_outputs)
         
-        # TODO: Add Weight step (input to hidden) and Weight step (hidden to output).
         # Weight step (input to hidden)
-        delta_weights_i_h += None
+        delta_weights_i_h += hidden_error_term * X[:,None]
         # Weight step (hidden to output)
-        delta_weights_h_o += None
+        delta_weights_h_o += output_error_term * hidden_outputs[:,None]
         return delta_weights_i_h, delta_weights_h_o
 
     def update_weights(self, delta_weights_i_h, delta_weights_h_o, n_records):
@@ -115,8 +114,8 @@ class NeuralNetwork(object):
             n_records: number of records
 
         '''
-        self.weights_hidden_to_output += None # update hidden-to-output weights with gradient descent step
-        self.weights_input_to_hidden += None # update input-to-hidden weights with gradient descent step
+        self.weights_hidden_to_output += self.lr * delta_weights_h_o / n_records # update hidden-to-output weights with gradient descent step
+        self.weights_input_to_hidden += self.lr * delta_weights_i_h / n_records # update input-to-hidden weights with gradient descent step
 
     def run(self, features):
         ''' Run a forward pass through the network with input features 
@@ -128,12 +127,12 @@ class NeuralNetwork(object):
         
         #### Implement the forward pass here ####
         # TODO: Hidden layer - replace these values with the appropriate calculations.
-        hidden_inputs = None # signals into hidden layer
-        hidden_outputs = None # signals from hidden layer
+        hidden_inputs = np.dot(features, self.weights_input_to_hidden) # signals into hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs) # signals from hidden layer
         
         # TODO: Output layer - Replace these values with the appropriate calculations.
-        final_inputs = None # signals into final output layer
-        final_outputs = None # signals from final output layer 
+        final_inputs = np.dot(hidden_outputs, self.weights_hidden_to_output) # signals into final output layer
+        final_outputs = final_inputs # signals from final output layer 
         
         return final_outputs
 
@@ -141,7 +140,144 @@ class NeuralNetwork(object):
 #########################################################
 # Set your hyperparameters here
 ##########################################################
-iterations = 100
-learning_rate = 0.1
-hidden_nodes = 2
+# initial
+
+#iterations = 100
+#learning_rate = 0.1
+#hidden_nodes = 2
 output_nodes = 1
+
+# 1st trial
+
+#Progress: 100.0% ... Training loss: 0.130 ... Validation loss: 0.232
+#Progress: 100.0% ... Training loss: 0.218 ... Validation loss: 0.393
+#iterations = 5000
+#learning_rate = 0.2
+#hidden_nodes = 2
+
+#Progress: 100.0% ... Training loss: 0.221 ... Validation loss: 0.375
+#iterations = 5000
+#learning_rate = 0.25
+#hidden_nodes = 2
+
+#Progress: 100.0% ... Training loss: 0.194 ... Validation loss: 0.288
+#iterations = 5000
+#learning_rate = 0.4
+#hidden_nodes = 2
+
+##Progress: 100.0% ... Training loss: 0.119 ... Validation loss: 0.205
+#iterations = 5000
+#learning_rate = 0.2
+#hidden_nodes = 4
+
+#Progress: 100.0% ... Training loss: 0.243 ... Validation loss: 0.405
+#iterations = 5000
+#learning_rate = 0.1
+#hidden_nodes = 8
+
+#Progress: 100.0% ... Training loss: 0.136 ... Validation loss: 0.253
+#iterations = 5000
+#learning_rate = 0.2
+#hidden_nodes = 8
+
+#Progress: 100.0% ... Training loss: 0.120 ... Validation loss: 0.244
+#iterations = 5000
+#learning_rate = 0.2
+#hidden_nodes = 16
+
+#Progress: 100.0% ... Training loss: 0.072 ... Validation loss: 0.165
+#Progress: 100.0% ... Training loss: 0.077 ... Validation loss: 0.180
+#iterations = 10000
+#learning_rate = 0.2
+#hidden_nodes = 16
+
+#Progress: 100.0% ... Training loss: 0.080 ... Validation loss: 0.182
+#iterations = 10000
+#learning_rate = 0.2
+#hidden_nodes = 24
+
+#Progress: 100.0% ... Training loss: 0.150 ... Validation loss: 0.269
+#iterations = 5000
+#learning_rate = 0.2
+#hidden_nodes = 32
+
+# 1st release
+#Progress: 100.0% ... Training loss: 0.095 ... Validation loss: 0.161
+#Progress: 100.0% ... Training loss: 0.067 ... Validation loss: 0.167
+#iterations = 10000
+#learning_rate = 0.2
+#hidden_nodes = 32
+
+#Progress: 100.0% ... Training loss: 0.068 ... Validation loss: 0.152
+#iterations = 10000
+#learning_rate = 0.25
+#hidden_nodes = 32
+
+#Progress: 100.0% ... Training loss: 0.125 ... Validation loss: 0.272
+#iterations = 5000
+#learning_rate = 0.2
+#hidden_nodes = 64
+
+#Progress: 100.0% ... Training loss: 0.061 ... Validation loss: 0.152
+#iterations = 10000
+#learning_rate = 0.2
+#hidden_nodes = 64
+
+#Progress: 100.0% ... Training loss: 0.071 ... Validation loss: 0.156
+#iterations = 15000
+#learning_rate = 0.15
+#hidden_nodes = 64
+
+# 2nd trial
+
+#Progress: 100.0% ... Training loss: 0.975 ... Validation loss: 1.367
+#iterations = 5000
+#learning_rate = 1
+#hidden_nodes = 32
+
+#Progress: 39.0% ... Training loss: 0.974 ... Validation loss: 1.367
+#iterations = 5000
+#learning_rate = 1
+#hidden_nodes = 64
+
+#Progress: 100.0% ... Training loss: 0.730 ... Validation loss: 1.183
+#iterations = 3000
+#learning_rate = 0.9
+#hidden_nodes = 32
+
+#Progress: 100.0% ... Training loss: 0.752 ... Validation loss: 1.217
+#iterations = 2000
+#learning_rate = 0.8
+#hidden_nodes = 32
+
+#Progress: 100.0% ... Training loss: 0.533 ... Validation loss: 0.618
+#iterations = 3000
+#learning_rate = 0.7
+#hidden_nodes = 32
+
+#Progress: 100.0% ... Training loss: 0.155 ... Validation loss: 0.333
+#iterations = 3000
+#learning_rate = 0.6
+#hidden_nodes = 32
+
+#Progress: 100.0% ... Training loss: 0.092 ... Validation loss: 0.184
+#iterations = 3000
+#learning_rate = 0.5
+#hidden_nodes = 32
+
+#Progress: 100.0% ... Training loss: 0.088 ... Validation loss: 0.213
+#Progress: 100.0% ... Training loss: 0.078 ... Validation loss: 0.161
+#Progress: 100.0% ... Training loss: 0.078 ... Validation loss: 0.161
+iterations = 5000
+learning_rate = 0.5
+hidden_nodes = 32
+
+#Progress: 100.0% ... Training loss: 0.099 ... Validation loss: 0.222
+#iterations = 3000
+#learning_rate = 0.4
+#hidden_nodes = 24
+
+#Progress: 100.0% ... Training loss: 0.080 ... Validation loss: 0.197
+#iterations = 3000
+#learning_rate = 0.4
+#hidden_nodes = 32
